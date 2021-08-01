7e0f405b3baa6b6ee81ed7a034ccb5b5e6b1a30c
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-01 01:29:51 -0500

    Add age to time_data

--- scripts/create_time_data.py
+++ scripts/create_time_data.py
@@ -3,7 +3,7 @@ from Class_list import *
 
 def clean_data(data=pd.DataFrame()):
     columns = data.columns
-    columns = columns.drop("Nombre completo")
+    columns = columns.drop(["Nombre completo", "Edad"])
     data = data.drop(columns, 1)
     return data
 
@@ -11,12 +11,13 @@ def clean_data(data=pd.DataFrame()):
 parameters = {"path data": "../data/",
               "file data": "dummy_data.csv",
               "file time": "time_data.csv"}
+test = test_list()
+tests = test.data.keys()
 swimmers = swimmers_data(parameters["path data"],
                          parameters["file data"])
 swimmers.data = clean_data(swimmers.data)
-test = test_list()
-tests = test.data.keys()
-time_data = pd.DataFrame(index=swimmers.data["Nombre completo"],
-                         columns=tests)
+time_data = swimmers.data
+time_data[list(tests)] = ""
 time_data.to_csv("{}{}".format(parameters["path data"],
-                               parameters["file time"]))
+                               parameters["file time"]),
+                 index=False)
